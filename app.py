cfdd230c8066da03a6aed6a8737d6823fcca0466
kawamura-t <takumikawamura1@gmail.com>
2023-04-13 00:32:27 +0900

    test

--- app.py
+++ app.py
@@ -5,13 +5,21 @@ import pathlib
 
 root =tk.Tk()
 
+def addApp():
+
+    filename = filedialog.askopenfilename(initialdir="/", filetype=(("exexutables", "*.exe"), ("all files", "*.*")))
+
+
+
+
+
 canvas = tk.Canvas(width=700, height=700,bg="#263D42")
 canvas.pack()
 
 frame = tk.Frame(root, bg="white")
 frame.place(relheight=0.8, relwidth=0.8, relx=0.1, rely=0.1)
 
-
-
+openFileButton = tk.Button(root, text="Open File", padx=10, pady=10, fg="white", bg="#263D42", command=addApp)
+openFileButton.pack()
 
 root.mainloop()
